e86b33e142759885f188e43778cda0224e4e30d0
Galvayra <voltexlicer@gmail.com>
2019-06-27 17:06:18 +0900

    build script

--- build.py
+++ build.py
@@ -4,11 +4,11 @@ import sys
 from os import path
 
 try:
-    import DialogSystem
+    import DialogueSystem
 except ImportError:
     sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
-from DialogSystem.dataset.dataHandler import DataHandler
+from DialogueSystem.dataset.dataHandler import DataHandler
 
 
 if __name__ == '__main__':
